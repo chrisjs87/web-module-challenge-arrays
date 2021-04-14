b183b495815811907728dcf1209e043e2814952d
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-04-14 12:57:20 -0500

    Initial commit with 2 solutions, test installs did not work

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
+function copy(array){
+    return array;
 }    
-
+// copy(originalFlavors);
 
 
 
@@ -63,9 +63,12 @@ Confirm that an array is exactly 31 flavors. Your function should accept:
 For Example: is31Flavors(originalFlavors) will return true if your code is working properly
 */
 
-
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(array){
+   if(array.length === 31){
+       return true;
+   }else {
+       return false;
+   }
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
